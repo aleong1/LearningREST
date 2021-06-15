7a196aa163cab4ed70f21c9ef9dfead7a2664395
Alexia <alexialeong5@gmail.com>
2021-06-15 16:04:58 -0400

    Added some controller functions

--- actuator-service/src/main/java/com/example/actuatorservice/HelloWorldController.java
+++ actuator-service/src/main/java/com/example/actuatorservice/HelloWorldController.java
@@ -11,14 +11,22 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class HelloWorldController {
 
     private static String hello = "Hello %s!";
+    private static String bye = "Bye %s!";
     private AtomicInteger counter = new AtomicInteger();
     // AtomicInteger is recommended to be used instead of int
     // in cases where there are multiple threads (connections?)
+    // so the counter will be synchronized across all threads
 
     @GetMapping("/hello-world")
     @ResponseBody
     public Greetings sayHello(@RequestParam(name = "name", required = false, defaultValue = "World") String name){
         return new Greetings(counter.incrementAndGet(), String.format(hello, name));
+    }
+
+    @GetMapping("/bye-world")
+    @ResponseBody
+    public Greetings sayBye(@RequestParam(name = "name", required = false, defaultValue = "World") String name){
+        return new Greetings(counter.incrementAndGet(), String.format(bye, name));
     }
 
 }
