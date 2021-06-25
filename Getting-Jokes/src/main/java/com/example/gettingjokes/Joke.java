8fbe6d9fbe772083535c5762b718909bea284bb8
Alexia <alexialeong5@gmail.com>
2021-06-25 13:32:36 -0400

    Created repository file

--- Getting-Jokes/src/main/java/com/example/gettingjokes/Joke.java
+++ Getting-Jokes/src/main/java/com/example/gettingjokes/Joke.java
@@ -1,9 +1,21 @@
 package com.example.gettingjokes;
 
+import javax.persistence.*;
+
+//POJO
+@Entity
+@Table(name = "jokes")
 public class Joke {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
+
+    @Column(name = "setup")
     private String setup;
+
+    @Column(name = "delivery")
     private String delivery;
 
     public Joke(int id, String setup, String delivery){
@@ -11,6 +23,8 @@ public class Joke {
         this.setup = setup;
         this.id = id;
     }
+
+    public Joke() { }
 
     public int getId() {
         return id;
