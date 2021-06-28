5a236432d1174016731347f8c3445b98c2ba6e1f
Alexia <alexialeong5@gmail.com>
2021-06-28 10:56:59 -0400

    added repository file

---
+++ Getting-Jokes/src/main/java/com/example/gettingjokes/GettingJokesRepository.java
@@ -0,0 +1,7 @@
+package com.example.gettingjokes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GettingJokesRepository extends JpaRepository<Joke, Integer>{
+
+}
