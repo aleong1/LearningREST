b9b24bc4ead0985428e3a81a01b55cdf32aaf61f
Alexia Leong <alexialeong5@gmail.com>
2021-06-30 12:20:05 -0400

    deleted a comment

--- Getting-Jokes/src/main/java/com/example/gettingjokes/GettingJokesJPAService.java
+++ Getting-Jokes/src/main/java/com/example/gettingjokes/GettingJokesJPAService.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.sql.*;
 import java.util.List;
 import java.util.Optional;
 
